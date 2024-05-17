808cb2b40a39d65e266ac72c2c2eeda200359b38
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-05-17 16:02:33 +0700

    sending http request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
 
   return (
     <Places
