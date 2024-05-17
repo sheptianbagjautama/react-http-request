3b2a8e3ea2d89dbe81cf9ccf4554b37b52e487e5
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-05-17 15:38:26 +0700

    preparing the App For Data Fetching

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,12 @@
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
